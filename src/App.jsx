ce45c288b5ec2b62469ab4ae56ba8239224b302c
Rohan K Joseph <131992408+RohanKJoseph@users.noreply.github.com>
2026-02-06 14:03:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ 
+ 
+ function App() {
+  return (
+     <main >
+      <div className="page1">
+        <button className="sm-gray-button">EXPORT CODE</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
